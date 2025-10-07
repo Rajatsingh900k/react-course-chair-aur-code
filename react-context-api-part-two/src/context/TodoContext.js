222a6a919f9b0e9fdd791ac65dbbd64afd87dea6
Rajatsingh900k <rajatsingh900k@gmail.com>
2025-10-07 23:14:45 +0530

    updated

--- react-context-api-part-two/src/context/TodoContext.js
+++ react-context-api-part-two/src/context/TodoContext.js
@@ -1,7 +1,19 @@
 import React from 'react'
 import { createContext, useContext } from 'react'
 
-export const TodoContext = createContext({})
+export const TodoContext = createContext({
+    todos: [
+        {
+            id:1,
+            todoTitle:"msd",
+            completed:false
+        }
+    ],
+    addTodo: (todo)=>{},
+    update:(id,todo)=>{},
+    delete:(id)=>{},
+    toggleComplete:(id)=>{}
+})
 
 export const useTodo = ()=>{
     return useContext(TodoContext)// which context we are talkign about, kinda reference.
